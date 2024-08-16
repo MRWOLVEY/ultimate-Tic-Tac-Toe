50a6b377becca79c8cf7f56e82ebb4f44f79d4ea
Mohammed Morra <wolfeyplays123@gmail.com>
2024-08-16 07:26:52 +0300

    added the shadows and the conditional classes

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -17,7 +17,7 @@ function Board() {
       <div className='grid grid-rows-[7rem_7rem_7rem] grid-cols-3 gap-1 bg-slate-800 p-3 h-fit rounded relative z-10'>
           {
               state.boxes.map((box,i)=>{
-                  return <Box className="" box_id={i} key={i} sizes={sizes}/>
+                  return <Box className="box" box_id={i} key={i} sizes={sizes}/>
               })
           }
           <div className="absolute inset-0 bg-transparent pointer-events-auto"></div>
--- src/Components/Box.jsx
+++ src/Components/Box.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState,useContext } from 'react'
 import Cell from './Cell'
 import cellsContext from "./cellsContext"
 import classNames from 'classnames'
+import "./box.css"
 
 function Box({children,box_id,sizes}) {
   const {state,dispatch,ACTIONS} = useContext(cellsContext);
@@ -22,20 +23,24 @@ function Box({children,box_id,sizes}) {
 
     if (state.boxes[box_id].won == undefined){
       return (
-      <div className={`grid grid-cols-3 bg-slate-800 h-fit rounded`} style={boxStyle}>
-      {
-        state.boxes[box_id].cells.map((cell,i)=>{
-          return <Cell key={i} box_id={box_id} id_={i} sizes={sizes}/>
-        })
-      }
-    </div>
+        <>
+            <div className={classNames('grid grid-cols-3 bg-slate-800 h-fit rounded',{'active_box':state.selectBox})} style={boxStyle}>
+              {
+                state.boxes[box_id].cells.map((cell,i)=>{
+                  return <Cell key={i} box_id={box_id} id_={i} sizes={sizes}/>
+                })
+              }
+            </div>
+        </>
       )
     }
     else{
       return (
-    <div className={`wining_grid bg-slate-50 rounded`} style={overlayStyle} >
-      <img src={state.boxes[box_id].url} className=""/>
-    </div>
+        <>
+          <div className={classNames(' bg-slate-50 rounded',{'wining_grid':state.selectBox})} style={overlayStyle} >
+            <img src={state.boxes[box_id].url} className=""/>
+          </div>
+        </>
       )
     }
 }
