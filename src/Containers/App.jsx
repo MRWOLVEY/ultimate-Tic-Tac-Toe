67d6636be8f9df9b6bf56c482056d72ee17f1dca
Mohammed Morra <wolfeyplays123@gmail.com>
2024-08-16 14:47:03 +0300

    created conditional rendering for the gameStatus state variable

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -37,10 +37,38 @@ const App = () => {
         turn.current = (turn.current === 'x') ? 'o' : 'x';
     }
 
+    // if (state.newGame) {
+    //     dispatch({type:ACTIONS.resetStateAndTurn})
+    //     return(
+    //         <cellsContext.Provider value={{state,dispatch,ACTIONS}}>
+    //             <div className="App h-lvh lg:px-32 xl:px-60 bg-gray flex flex-col lg:flex-row items-center justify-between">
+    //                 <div className="message flex flex-col">
+    //                     <h1 className="text-3xl font-bold">please select a box to start the game</h1>
+    //                     <div className="flex flex-row justify-around">
+    //                         <div className="left">
+    //                             <span className="text-xl">player 1 X</span>
+    //                         </div>
+    //                         <div className="right">
+    //                         <span className="text-xl">player 2 O</span>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //                 <Board/>
+    //             </div>
+    //         </cellsContext.Provider>
+    //     )
+    // }
+
     return (
         <cellsContext.Provider value={{state,dispatch,ACTIONS}}>
             <div className="App h-lvh lg:px-32 xl:px-60 bg-gray flex flex-col lg:flex-row items-center justify-between">
-                <Box className="" box_id={0} sizes={sizes} />
+                {state.gameStatus=='newGame'&&<div className="message flex flex-col justify-center h-100">
+                    <h1 className="text-2xl font-bold">please select a box to start the game</h1>
+                </div>}
+                {state.gameStatus=='select'&&<div className="message flex flex-col justify-center h-100">
+                    <h1 className="text-2xl font-bold">please select a box that is highlighted in white</h1>
+                </div>}
+                {state.gameStatus=='turn'&&<Box className="" box_id={state.currentBox} sizes={sizes} />}
                 <Board/>
             </div>
         </cellsContext.Provider>
