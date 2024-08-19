ee05015938190c88d6b5c5440628c7a749df48e8
Mohammed Morra <wolfeyplays123@gmail.com>
2024-08-19 10:44:38 +0300

    pallette 1

--- src/Components/Box.jsx
+++ src/Components/Box.jsx
@@ -25,7 +25,7 @@ function Box({children,box_id,sizes,boardItem}) {
       return (
         <div>
         {state.gameStatus=='turn'&&!boardItem&&<h1 className='text-center text-3xl mb-3'>player {state.turn} turn</h1>}
-          <div className={classNames('grid grid-cols-3 bg-slate-800 h-fit rounded relative transition ',{'hover:scale-105 active:scale-95 active_box':boardItem&&state.boxes[box_id].won == undefined&&state.gameStatus=='newGame'||boardItem&&state.boxes[box_id].won == undefined&&state.gameStatus=='select','current_box':boardItem&&state.currentBox==box_id&&state.gameStatus=='turn'})} style={boxStyle}>
+          <div className={classNames('grid grid-cols-3 bg-green-800 h-fit rounded relative transition ',{'hover:scale-105 active:scale-95 active_box':boardItem&&state.boxes[box_id].won == undefined&&state.gameStatus=='newGame'||boardItem&&state.boxes[box_id].won == undefined&&state.gameStatus=='select','current_box':boardItem&&state.currentBox==box_id&&state.gameStatus=='turn'})} style={boxStyle}>
             {
               state.boxes[box_id].cells.map((cell,i)=>{
                 return <Cell key={i} box_id={box_id} id_={i} sizes={sizes}/>
--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -37,13 +37,13 @@ const App = () => {
 
     return (
         <cellsContext.Provider value={{state,dispatch,ACTIONS}}>
-            <div className="App min-h-lvh lg:px-32 xl:px-60 pb-4 bg-gray flex flex-col gap-8">
-                <h1 className="text-4xl font-bold flex items-center justify-center text-center h-24">Ultimate Tic-Tac-Toe</h1>
+            <div className="App min-h-lvh lg:px-32 xl:px-60 pb-4 bg-amber-200 flex flex-col gap-8">
+                <h1 className="text-5xl font-bold flex items-center justify-center text-center text-orange-500 h-24">Ultimate Tic-Tac-Toe</h1>
                 <div className="Game flex flex-col gap-4 lg:flex-row items-center justify-between w-100">
                     {state.gameStatus=='newGame'&&<div className="message flex flex-col gap-3 justify-center max-w-80 h-100">
                         <h1 className="text-4xl font-bold text-center">please select a box to start the game</h1>
-                        <p className="text-center text-3xl">score</p>
-                        <div className="flex justify-between">
+                        <p className="text-center text-3xl border-2 w-fit border-orange-500 p-1 rounded">score</p>
+                        <div className="flex justify-between border-2 border-orange-500 p-1 rounded">
                             <span>player X <br />{state.scores[0]}</span>
                             <span>player O <br />{state.scores[1]}</span>
                         </div>
